e28b919a53957eb2d87b010a6bb8a8c691b22cb8
GeorgeGUS <georgegus1@gmail.com>
2018-02-16 15:45:09 +0300

    Доделал реализацию слайдера для перетаскивания мышкой и поправил стили слайдера

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,4 +1,5 @@
 (function () {
+  var TABLET_WIDTH = 768;
   var slider = document.querySelector(".slider");
   var scale = slider.querySelector(".slider__scale");
   var grip = scale.querySelector(".slider__grip");
@@ -6,18 +7,13 @@
   var after = slider.querySelector(".slider__image--after");
   var btnBefore = slider.querySelector(".slider__btn--before");
   var btnAfter = slider.querySelector(".slider__btn--after");
+  var sliderWidth,
+    scaleWidth,
+    gripWidth;
 
   var getElemWidth = function (elem) {
     return parseInt(getComputedStyle(elem).width, 10);
   };
-
-  var sliderWidth = getElemWidth(slider);
-  var gripWidth = getElemWidth(grip);
-
-  window.addEventListener("resize", function () {
-    sliderWidth = getElemWidth(slider);
-    gripWidth = getElemWidth(grip);
-  });
 
   btnBefore.onclick = function (evt) {
     evt.preventDefault();
@@ -44,11 +40,9 @@
     return box.left + pageXOffset;
   };
 
-  grip.onmousedown = function (evtDown) {
-    // var afterWidth = getElemWidth(after);
-    // var gripCoords = getCoords(grip);
-    // var scaleCoords = getCoords(scale);
-    var gripCoords = getComputedStyle(elem).marginLeft;
+  var gripDownHandler = function (evtDown) {
+    var gripCoords = getCoords(grip);
+    var scaleCoords = getCoords(scale);
     grip.style.transition = "none";
 
     var shiftX = evtDown.pageX - gripCoords;
@@ -59,22 +53,54 @@
       if (newLeft < 0) {
         newLeft = 0;
       }
-      var rightEdge = scale.innerWidth - grip.offsetWidth;
+
+      var rightEdge = scaleWidth - gripWidth;
       if (newLeft > rightEdge) {
         newLeft = rightEdge;
       }
-      var gripPos = newLeft / rightEdge * 100;
 
-      grip.style.left = newLeft + "px";
+      var gripValue = newLeft / rightEdge * 100;
+      grip.style.marginLeft = newLeft + "px";
 
-      after.style.clip = "rect(auto, auto, auto, " + (sliderWidth - sliderWidth * gripPos / 100) + "px)";
+      before.style.width = (100 - gripValue) + "%";
+      after.style.width = gripValue + "%";
     };
 
     document.onmouseup = function () {
       document.onmousemove = document.onmouseup = null;
-      grip.style.transition = "all 0.3s ease-out";
+      grip.style.transition = "margin-left 0.3s ease-out";
     };
 
     return false;
   };
+
+  var addGripHandlers = function () {
+    grip.addEventListener("mousedown", gripDownHandler);
+  };
+
+  var removeGripHandlers = function () {
+    grip.removeEventListener("mousedown", gripDownHandler);
+  };
+
+
+  var initialize = function() {
+    var viewport = document.documentElement.clientWidth || window.innerWidth;
+
+    if (viewport >= TABLET_WIDTH) {
+      addGripHandlers();
+    } else {
+      removeGripHandlers();
+    }
+
+    sliderWidth = getElemWidth(slider);
+    scaleWidth = getElemWidth(scale);
+    gripWidth = getElemWidth(grip);
+
+    before.style.width = "";
+    after.style.width = "";
+    grip.style.marginLeft = "";
+  };
+
+  window.addEventListener("load", initialize);
+  window.addEventListener("resize", initialize);
 })();
